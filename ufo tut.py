585255fd3c9d60edf0e412b9bc05e2198d3d75c2
MatthewJGray <MatthewJGray>
2024-03-06 10:19:42 +0000

    titles

--- ufo tut.py
+++ ufo tut.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC #### this code connects Databricks to an Azure Blob Storage container named ufo-data and makes it accessible as if it were a folder (/mnt/ufo-data) within Databricks. It also ensures secure access by providing the necessary account key from Databricks secrets.
+
+# COMMAND ----------
+
 dbutils.fs.mount(
     source='wasbs://ufo-data@moviesdatatutorial.blob.core.windows.net',
     mount_point='/mnt/ufo-data',
@@ -7,8 +12,18 @@ dbutils.fs.mount(
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC #### This code snippet uses the %fs magic command in Databricks to list the files and folders within the mounted Azure Blob Storage container named /mnt/ufo-data. it's asking Databricks to show what files and folders are available within the ufo-data container that has been mounted at /mnt/ufo-data2.
+
+# COMMAND ----------
+
 # MAGIC %fs
 # MAGIC ls "/mnt/ufo-data"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ####  this line of code is using a tool called Spark to read a CSV file named "ufo-sightings-transformed.csv". It specifies that the file has headers (the first row contains column names). Then, it loads this CSV file into a variable named "ufo" so that it can be used for further processing
 
 # COMMAND ----------
 
@@ -20,7 +35,17 @@ ufo.limit(15).show()
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ####  ufo.printSchema() is a command in Spark that displays the structure of the DataFrame named "ufo". It shows the data types of each column in the DataFrame, helping you understand the layout of the data you're working with. This can be useful for checking the types of data in your DataFrame and ensuring they match your expectations.
+
+# COMMAND ----------
+
 ufo.printSchema()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ####  these imports provide essential functionalities for working with data in PySpark DataFrames, allowing you to reference columns and define their data types
 
 # COMMAND ----------
 
@@ -29,11 +54,27 @@ from pyspark.sql.types import IntegerType, DoubleType, BooleanType, DateType
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ####  this line of code is ensuring that the values in the "Encounter_Duration" column of the DataFrame ufo are interpreted as integers.
+
+# COMMAND ----------
+
 ufo = ufo.withColumn("Encounter_Duration", col("Encounter_Duration").cast(IntegerType()))
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC #### Casting the "Year" column to DateType() is not appropriate because "Year" is typically represented as an integer in datasets, not a date. Casting it to DateType() would result in an error.
+
+# COMMAND ----------
+
 ufo = ufo.withColumn("Year", col("Year").cast(IntegerType()))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### this code essentially converts the 'date_documented' column from its original format to a date format.
+# MAGIC ##### The new column is derived from the existing 'date_documented' column but with a different data type or format.
 
 # COMMAND ----------
 
@@ -43,6 +84,11 @@ ufo= ufo.withColumn('date_documented',F.to_date(ufo.date_documented))
 # COMMAND ----------
 
 ufo.write.mode("overwrite").option("header",'true').csv("/mnt/ufo-data/transformed/ufo")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### By doing this, you have the ability to run SQL queries against this DataFrame using Spark SQL. Once created as a temporary table, you can reference it in SQL queries using the name "ufo_table", allowing you to perform SQL operations like SELECT, JOIN, etc., on the DataFrame
 
 # COMMAND ----------
 
@@ -57,6 +103,11 @@ result = spark.sql("""
     ORDER BY CountOfSightings DESC
 """)
 result.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### The three quotation marks (""") are used to show a multi-line string literal in Python. This allows you to write a string that covers multiple lines without having to use newline characters (\n).
 
 # COMMAND ----------
 
@@ -114,6 +165,11 @@ display(longest_encounter_per_country)
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ##### displays UFO sighting data on a map, filtering out invalid or missing location data before visualization.
+
+# COMMAND ----------
+
 # Filter out rows with missing or invalid latitude/longitude values
 valid_location_data = ufo.filter((col("latitude").isNotNull()) & (col("longitude").isNotNull()))
 
@@ -125,8 +181,17 @@ display(locations)
 
 # COMMAND ----------
 
+#### Latitude and longitude coordinates are  represented as floating-point numbers. Casting them to DoubleType() means that they are in the correct format for performing map analysis or visualization tasks, such as plotting points on maps.
+
+# COMMAND ----------
+
 ufo = ufo.withColumn("latitude", col("latitude").cast(DoubleType()))
 ufo = ufo.withColumn("longitude", col("longitude").cast(DoubleType()))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### this line of code is an essential step in the data pipeline, allowing you to save the transformed DataFrame ufo to a CSV file for further analysis, sharing, or storage.
 
 # COMMAND ----------
 
@@ -142,6 +207,12 @@ locations = valid_location_data.select("latitude", "longitude", "Country")
 
 # Display the UFO sightings on a map
 display(locations)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### just an edit with cluster markers added 
+# MAGIC
 
 # COMMAND ----------
 
